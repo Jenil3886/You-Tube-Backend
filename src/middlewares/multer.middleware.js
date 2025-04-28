982a7fac31e93c7f448bdbd3cad49d3fd8f5eca1
Jenil3886 <119419862+vaibhav-karmadhi@users.noreply.github.com>
2025-04-28 09:01:31 +0530

    small changes

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -12,7 +12,7 @@ const storage = multer.diskStorage({
 
 export const upload = multer({
   storage,
-  limits: { fileSize: 10 * 1024 * 1024 }, // Limit file size to 10MB
+  limits: { fileSize: 10 * 1024 * 1024 },
   fileFilter: (req, file, cb) => {
     const allowedMimeTypes = ["image/jpeg", "image/png", "video/mp4"];
     if (!allowedMimeTypes.includes(file.mimetype)) {
